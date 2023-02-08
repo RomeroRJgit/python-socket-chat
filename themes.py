29e1f2e259c26c373d81fb14ff1251f4baeec985
Ryan Romero <rydev.romero@gmail.com>
2023-02-08 00:33:35 -0500

    Add custom derived Entry with placeholder
    Created a child class of ttk.Entry in order to have
    a flexible placeholder

--- themes.py
+++ themes.py
@@ -3,12 +3,41 @@ from tkinter import ttk
 from tkinter.ttk import *
 
 
+class EntryXL(ttk.Entry):
+    placeholder_color = '#888888'
+    text_color = '#454142'
+
+    def __init__(self, master=None, widget=None, placeholder='', **kw):
+        super().__init__(master)
+        self.placeholder = placeholder if placeholder != '' else ''
+        self.valid_input = self.get()
+        self.bind('<Visibility>', self.show_placeholder)
+        self.bind('<FocusIn>', self.hide_placeholder)
+        self.bind('<FocusOut>', self.show_placeholder)
+
+    def hide_placeholder(self, event):
+        if self.get() == self.placeholder:
+            self.delete(0, len(self.get()))
+            self['foreground'] = self.text_color
+
+    def show_placeholder(self, event):
+        if self.get() == "":
+            self.insert(0, self.placeholder)
+            self['foreground'] = self.placeholder_color
+
+    def get_valid_input(self):
+        if self.get() == '' or self.get() == self.placeholder:
+            return ''
+        return self.get()
+
+
+
 class Modern:
-    # fields
+    # Fields
     theme_name = 'modern'
     style = None
 
-    # general styles
+    # General styles
     font = 'TkDefaultFont'
     bg_color = '#3f444A'
     mg_color = '#3A3938'
@@ -55,19 +84,20 @@ class Modern:
         self.style.configure('TFrame', background=self.bg_color, relief='flat')
 
         self.style.configure('h1.TLabel', background=self.bg_color, foreground=self.fg_color, font=('Raleway', 32),
-                             padding=30, relief='flat')
+                             padding=0, relief='flat')
         self.style.configure('h2.TLabel', background=self.bg_color, foreground=self.fg_color, font=('Raleway', 24),
                              padding=30, relief='flat')
         self.style.configure('p.TLabel', background=self.bg_color, foreground=self.text_color, font=(self.font, 14),
                              relief='flat')
 
-        self.style.configure('input.TLabel', background='#bbbbbb', foreground=self.text_color, font=(self.font, 24),
+        self.style.configure('input.TLabel', background='#cac5bb', foreground='#999999', font=(self.font, 24),
                              relief='flat')
 
-        self.style.configure('entry.TFrame', background='#bbbbbb', foreground=self.text_color, font=(self.font, 24),
+        self.style.configure('entry.TFrame', background='#cac5bb', font=(self.font, 24),
                              relief='flat')
 
-        self.style.configure('TButton', background='#333333', foreground=self.fg_color, font=('font', 14), padding='15 10',
+        self.style.configure('TButton', background='#333333', foreground=self.fg_color, font=('font', 14),
+                             padding='15 10',
                              relief='flat')
 
         self.style.configure('button-large.TButton', background='#333333', foreground=self.fg_color, font=('font', 24),
@@ -75,4 +105,3 @@ class Modern:
                              relief='flat')
 
         return self.style
-
